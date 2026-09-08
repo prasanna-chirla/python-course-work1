f86cecc3a0bfee3d1cf5cdcac86be6a04ae80ee9
Prasanna Chirla <prasannachirla9@gmail.com>
2026-09-08 09:20:37 +0530

    conditional statements(if and else)

---
+++ Day-10/if.py
@@ -0,0 +1,17 @@
+#seller 
+'''sales=int(input("enter the sales:"))
+if sales>1000:
+    print("best seller")'''
+
+#account validation
+'''eli_acc=eval(input("eligible account:"))
+ver_sub=eval(input("meta verified subscription:"))
+if eli_acc and ver_sub:
+    print("verified badge granted")'''
+
+#zepto rain delivery charges
+'''rain_status=eval(input("enter rain status"))
+if rain_status:
+    print("extra rain charges applied")'''
+
+
---
+++ Day-10/ifelse.py
@@ -0,0 +1,22 @@
+#username and password login
+'''username=input("enter username")
+password=input("enter password")
+if username=="admin" and password=="admin123":
+    print("login successful")
+else:
+    print("invalid")'''
+
+#product search
+'''products=["laptop","mouse","bottle","pen"]
+search=input("enter product name")
+if search in products:
+    print(f'{search} found')
+else:
+     print(f'{search} not found')'''
+
+#delivery charge calculation
+'''bill=int(input("enter bill: "))
+if bill>99:
+    print("bill is: ",bill)
+else:
+    print("bill+delivery: ",bill+30)'''
